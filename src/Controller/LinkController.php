57d16131aa8d28b613ecc304359f96ab87e3bf42
akrilo <kgavinovich@gmail.com>
2024-03-10 09:00:46 +0500

    first commit

---
+++ src/Controller/LinkController.php
@@ -0,0 +1,74 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Link;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+
+class LinkController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(): Response
+    {
+        return $this->render('index.html.twig');
+    }
+
+    /**
+     * @Route("/create-short-link", name="create_short_link", methods={"POST"})
+     */
+    public function createShortLink(Request $request): Response
+    {
+        $originalUrl = $request->request->get('original_url');
+
+        $link = new Link();
+        $link->setOriginalUrl($originalUrl);
+        $link->setShortUrl($this->generateShortUrl());
+        $link->setExpirationDate(new \DateTimeImmutable('+1 week')); // Настройка времени действия ссылки
+        $link->setCreatedAt(new \DateTimeImmutable());
+
+        $this->entityManager->persist($link);
+        $this->entityManager->flush();
+
+        return $this->json(['short_url' => $link->getShortUrl()]);
+    }
+
+    /**
+     * @Route("/{shortUrl}", name="redirect_short_link", methods={"GET"})
+     */
+    public function redirectShortLink($shortUrl): Response
+    {
+        $linkRepository = $this->entityManager->getRepository(Link::class);
+        $link = $linkRepository->findOneBy(['shortUrl' => $shortUrl]);
+
+        if (!$link || $link->getExpirationDate() < new \DateTimeImmutable()) {
+            return new RedirectResponse('/');
+        }
+
+        return $this->redirect($link->getOriginalUrl());
+    }
+
+    private function generateShortUrl(): string
+    {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $randomString = '';
+        $length = 6;
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, strlen($characters) - 1)];
+        }
+        return $randomString;
+    }
+}
---
+++ src/Entity/Link.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LinkRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LinkRepository::class)]
+class Link
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $originalUrl = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $shortUrl = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $expirationDate = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOriginalUrl(): ?string
+    {
+        return $this->originalUrl;
+    }
+
+    public function setOriginalUrl(string $originalUrl): static
+    {
+        $this->originalUrl = $originalUrl;
+
+        return $this;
+    }
+
+    public function getShortUrl(): ?string
+    {
+        return $this->shortUrl;
+    }
+
+    public function setShortUrl(string $shortUrl): static
+    {
+        $this->shortUrl = $shortUrl;
+
+        return $this;
+    }
+
+    public function getExpirationDate(): ?\DateTimeImmutable
+    {
+        return $this->expirationDate;
+    }
+
+    public function setExpirationDate(?\DateTimeImmutable $expirationDate): static
+    {
+        $this->expirationDate = $expirationDate;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
